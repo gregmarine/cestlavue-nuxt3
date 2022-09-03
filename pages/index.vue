bc238c4afb0616ad87a7b8b5601a2ab910ecca0f
Greg Marine <gregmarine@hey.com>
2022-09-03 19:51:11 -0400

    Switched to using useHead composable to add the Netlify Identity widget

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
-    <Head>
-      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"/>
-    </Head>
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup>
+useHead({
+  script: [
+    {
+      src: 'https://identity.netlify.com/v1/netlify-identity-widget.js'
+    }
+  ]
+})
+</script>
