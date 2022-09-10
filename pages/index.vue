ea396e992445f91cf882194d2837eec364e3219a
Greg Marine <gregmarine@hey.com>
2022-09-10 14:27:43 -0400

    Updating the main index

--- pages/index.vue
+++ pages/index.vue
@@ -9,8 +9,8 @@
       <div class="hero-overlay bg-opacity-60"></div>
       <div class="hero-content text-center text-neutral-content">
         <div class="max-w-md">
-          <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-          <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <h1 class="mb-5 text-5xl font-bold"><p>Greg Marine</p> <p>"The Website"</p></h1>
+          <p class="mb-5">On this site, you'll find a collection of my writing, photography, art, and recipes. Thank you for dropping by!</p>
           <button class="btn btn-primary">Get Started</button>
         </div>
       </div>
