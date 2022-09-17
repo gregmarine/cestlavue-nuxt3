253a024c2a63a052c58bc230c41a6b7aa737e296
Greg Marine <gregmarine@hey.com>
2022-09-17 16:40:28 -0400

    Fixing padding and margins for blog posts

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -3,8 +3,8 @@
     <ContentDoc v-slot="{ doc }">
       <Header :title="`Blog: ${ doc.title }`" />
 
-      <div class="container mx-auto h-full max-h-screen overflow-y-auto pt-8">
-        <ContentRenderer :value="doc" class="space-y-8" />
+      <div class="container px-4 md:mx-auto h-full max-h-screen overflow-y-auto pt-8">
+        <ContentRenderer :value="doc" class="space-y-8 mb-8" />
       </div>
     </ContentDoc>
   </div>
