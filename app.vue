7053efe574b252fd3893a7d2e52d42bdf99ee2ea
Greg Marine <gregmarine@hey.com>
2022-09-18 14:38:51 -0400

    More updates to meta tags

--- app.vue
+++ app.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <Head>
-      <Meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1 maximum-scale=1, viewport-fit=cover, user-scalable=no" />
+      <Meta name="viewport" content="viewport-fit=cover,width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
       <Meta name="format-detection" content="telephone=no" />
+      <Meta name="msapplication-tap-highlight" content="no" />
       <Meta name="apple-mobile-web-app-capable" content="yes" />
       <Meta name="apple-mobile-web-app-title" :content="settings.title" />
       <Meta name="apple-mobile-web-app-status-bar-style" content="black" />
