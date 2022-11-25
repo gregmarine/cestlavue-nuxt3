7374ff9e1f9fbd70afa047663436101b9ba1c40f
Greg Marine <gregmarine@hey.com>
2022-11-25 15:18:29 -0500

    Upgrading dependencies...especially to Nuxt 3 stable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,9 @@
-import { defineNuxtConfig } from 'nuxt'
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/content',
   ],
-  content: {
-    
-  },
   css: [
     "assets/css/styles.css"
   ]
