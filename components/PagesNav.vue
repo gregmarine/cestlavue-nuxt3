91259aae9bffac672021daacf106cc7eaaa816f4
Greg Marine <gregmarine@hey.com>
2022-11-27 14:01:06 -0500

    Fixing bottom margin for pages navigation

--- components/PagesNav.vue
+++ components/PagesNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-3 gap-4">
+  <div class="grid grid-cols-3 gap-4 mb-16">
     <div>
       <NuxtLink v-if="prev" :to="prev._path" class="inline-flex">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mt-1" fill="none" viewBox="0 0 512 512"><title>ionicons-v5-a</title><polyline points="244 400 100 256 244 112" style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px"/><line x1="120" y1="256" x2="412" y2="256" style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px"/></svg>
