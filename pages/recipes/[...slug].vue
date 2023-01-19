09854166ae389fa308a924c20e221dd29368b3fb
Greg Marine <gregmarine@hey.com>
2023-01-19 17:34:56 -0500

    Fixing recipe image justification on large screens

--- pages/recipes/[...slug].vue
+++ pages/recipes/[...slug].vue
@@ -28,10 +28,12 @@
       <Header :title="`${ settings.title }: ${ doc.title }`" />
 
       <div class="px-4 mx-auto h-full max-h-screen overflow-y-auto mb-16 prose dark:prose-invert">
-        <div v-if="doc.featured_image" class="card w-full lg:w-1/2 shadow-xl mb-8">
-          <figure>
-            <img :src="doc.featured_image" :alt="doc.title" />
-          </figure>
+        <div v-if="doc.featured_image" class="flex justify-center">
+          <div class="card w-full lg:w-1/2 shadow-xl mb-8">
+            <figure>
+              <img :src="doc.featured_image" :alt="doc.title" />
+            </figure>
+          </div>
         </div>
         
         <ContentRenderer :value="doc" class="space-y-8 mb-8" />
